e386e7f8cfeff1c511e4f42b240ff27fbaff8481
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-09-07 17:22:17 +0100

    book and input book

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,7 +13,26 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
   `;
+
 
   
 
