beea6e4de2cc75f82ea7e2458b3e18d6d2adf3d3
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-09-07 23:28:23 +0100

    read me

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
 import {
   ApolloClient,
   InMemoryCache,
